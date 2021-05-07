fa6cb8544311e987ce2ebbe43d85102bbb62868b
Petitenelly <82060745+Petitenelly@users.noreply.github.com>
2021-05-07 02:49:52 +0200

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,42 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Doctor.destroy_all
+Patient.destroy_all
+Appointment.destroy_all
+
+specialties = Specialty.create([{name:"Allergology"}, {name:"Cardiology"}, {name:"Dermatology"}, {name:"Endocrinology"}, {name:"General Medicine"}, {name:"Gastroenterology"}, {name:"General Practice"}, {name:"Geriatrics"}, {name:"Hematology"}, {name:"Immunology"}])
+
+10.times do
+    c = City.create(name: Faker::Address.city, zip_code: Faker::Address.zip_code)
+    p = Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, city: c)
+    d = Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, specialty:Faker::Job.title , zip_code: Faker::Address.zip_code, city: c)
+    rdv = Appointment.new(date: Faker::Date.forward(days: 30))
+    rdv.doctor = d
+    rdv.patient = p
+    rdv.city = c
+    rdv.save
+  end
+
+
 =begin
 specialties = Specialty.create([{name:"Allergology"}, {name:"Cardiology"}, {name:"Dermatology"}, {name:"Endocrinology"}, {name:"General Medicine"}, {name:"Gastroenterology"}, {name:"General Practice"}, {name:"Geriatrics"}, {name:"Hematology"}, {name:"Immunology"}, {name:"Infectious diseases"}, {name:"Internal medicine"}, {name:"Laboratory medicine"}, {name:"Nephrology"}, {name:"Neuropsychiatry"}, {name:"Neurology"}, {name:"Neurosurgery"}, {name:"Nuclear medicine"}, {name:"Obstetrics and gynaecology"}, {name:"Ophthalmology"}, {name:"Dental surgery"}, {name:"Orthopaedics"}, {name:"Otorhinolaryngology"}, {name:"Paediatrics"}, {name:"Pharmacology"}, {name:"Plastic surgery"}, {name:"Podiatric surgery"}, {name:"Preventive medicine"}, {name:"Psychiatry"}, {name:"Public health"},{name:"Radiation Oncology"}, {name:"Radiology"}, {name:"Respiratory medicine"}, {name:"Rheumatology"}, {name:"Stomatology"}, {name:"Thoracic surgery"}, {name:"Tropical medicine"}, {name:"Urology"}, {name:"Vascular surgery"}, {name:"Venereology"}])
 =end
 
 
-require 'faker'
-
-10.times do
-  doctor = Doctor.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, zip_code: Faker::Address.zip
-  )
-end
-puts "10 docteurs ont été créés"
-
-10.times do
-  patient = Patient.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name
-  )
-end
-puts "10 patients ont été créés"
-
-10.times do
-  appointment = Appointment.create(doctor_id: rand(1..25), patient_id: rand(1..50), date: Faker::Date.in_date_period 
-)                                
-end
-puts "10 RDV ont été créés"
-
-=begin
-10.times do
-    city = City.create(city: Faker::Address.city, doctor_id: rand(1..25), patient_id: rand(1..50), appointment_id:rand(1..25)
-  )
-  end
-  puts "10 villes ont été créés"
-
-  10.times do
-  specialty = Specialty.create(specialty: Faker::Job.title, doctor_id: rand(1..25), appointments_id: rand(1..25)
-)
-end
-puts "10 spécialités ont été créés"
-
-=end
